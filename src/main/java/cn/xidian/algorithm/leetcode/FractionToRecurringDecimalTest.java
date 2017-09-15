42855d2c8d90c02dd7b7cb7628efa3fa4ab4474d
XiDian-ChenMiao <786078509@qq.com>
2017-09-15 21:00:16 +0800

    add a string algorithm

---
+++ src/main/java/cn/xidian/algorithm/leetcode/FractionToRecurringDecimalTest.java
@@ -0,0 +1,33 @@
+package cn.xidian.algorithm.leetcode;
+import java.util.Map;
+import java.util.HashMap;
+
+public class FractionToRecurringDecimalTest {
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0)
+            return "0";
+        StringBuilder result = new StringBuilder("");
+        if (numerator < 0 && denominator > 0 || (numerator > 0 && denominator < 0))
+            result.append("-");
+        long n = Math.abs(Long.parseLong("" + numerator)), d = Math.abs(Long.parseLong("" + denominator));
+        result.append(n / d);
+        long rest = n % d;
+        if (rest == 0)
+            return result.toString();
+        else
+            result.append(".");
+        Map<Long, Integer> map = new HashMap<Long, Integer>();
+        while (rest != 0) {
+            if (map.containsKey(rest)) {
+                result.insert(map.get(rest), "(");
+                result.append(")");
+                break;
+            }
+            map.put(rest, result.length());
+            rest *= 10;
+            result.append(rest / d);
+            rest = rest % d;
+        }
+        return result.toString();
+    }
+}
