23659fa3580c69d798493ad0c88d8f7f8a6598d6
ChenMiao <786078509@qq.com>
2021-08-14 22:20:15 +0800

    leetcode.965 solve to check single value for binary-tree.

---
+++ src/main/java/cn/xidian/algorithm/leetcode/SingleValueTree.java
@@ -0,0 +1,29 @@
+package cn.xidian.algorithm.leetcode;
+
+/**
+ * @author chenmiao
+ * @version 1.0.0
+ * @title: SingleValueTree
+ * @description: LeetCode.965 单值二叉树判定
+ * @date 8/14/21 10:16 PM
+ */
+public class SingleValueTree {
+    /**
+     * 判定函数
+     *
+     * @param root
+     * @return
+     */
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (root.left != null && root.left.val != root.val) {
+            return false;
+        }
+        if (root.right != null && root.right.val != root.val) {
+            return false;
+        }
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
+}
